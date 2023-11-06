e3ceeb5333b642e9e09444800f834f87eb59278a
appsterlight <appsterlight@gmail.com>
2023-11-06 03:54:09 +0200

    Add @AllArgsConstructor annotation to Company entity

--- src/main/java/co/inventorsoft/academy/hibernate/model/Company.java
+++ src/main/java/co/inventorsoft/academy/hibernate/model/Company.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 @EqualsAndHashCode
 @ToString
