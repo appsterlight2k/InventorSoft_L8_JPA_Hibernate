a700378f9aaadfba4e331a9e0bafa780f77a58a3
appsterlight <appsterlight@gmail.com>
2023-11-06 02:51:04 +0200

    Add custom DaoException and ServiceException classes

---
+++ src/main/java/co/inventorsoft/academy/hibernate/exception/ServiceException.java
@@ -0,0 +1,7 @@
+package co.inventorsoft.academy.hibernate.exception;
+
+public class ServiceException extends RuntimeException {
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+}
