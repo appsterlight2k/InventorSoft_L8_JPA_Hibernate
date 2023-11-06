6e61b4c12b92287bf0cb2b5415724c69388fd86c
appsterlight <appsterlight@gmail.com>
2023-11-06 03:10:15 +0200

    Refactor CompanyDao and EmployeeDao to extend generic Dao interface; remove redundant method declarations

--- src/main/java/co/inventorsoft/academy/hibernate/dao/CompanyDAO.java
+++ src/main/java/co/inventorsoft/academy/hibernate/dao/CompanyDAO.java
@@ -2,10 +2,6 @@ package co.inventorsoft.academy.hibernate.dao;
 
 import co.inventorsoft.academy.hibernate.model.Company;
 
-public interface CompanyDAO {
-    void saveCompany(Company company);
-    Company findById(Long id);
-    void updateCompany(Company company);
-    void deleteCompany(Company company);
+public interface CompanyDAO extends Dao<Company> {
 
 }
--- src/main/java/co/inventorsoft/academy/hibernate/dao/EmployeeDAO.java
+++ src/main/java/co/inventorsoft/academy/hibernate/dao/EmployeeDAO.java
@@ -2,9 +2,6 @@ package co.inventorsoft.academy.hibernate.dao;
 
 import co.inventorsoft.academy.hibernate.model.Employee;
 
-public interface EmployeeDAO {
-    void saveEmployee(Employee employee);
-    Employee findById(Long id);
-    void updateEmployee(Employee employee);
-    void deleteEmployee(Employee employee);
+public interface EmployeeDAO extends Dao<Employee> {
+
 }
