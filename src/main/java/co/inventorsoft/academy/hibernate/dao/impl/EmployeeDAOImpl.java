3bb2fafefdd92acf72d645014dc38833835e17dc
appsterlight <appsterlight@gmail.com>
2023-11-06 03:14:12 +0200

    Refine CompanyDaoImpl and EmployeeDaoImpl to inherit from AbstractDao; Utilize super() with entity class in constructors; Remove redundant method implementations

--- src/main/java/co/inventorsoft/academy/hibernate/dao/impl/EmployeeDAOImpl.java
+++ src/main/java/co/inventorsoft/academy/hibernate/dao/impl/EmployeeDAOImpl.java
@@ -1,70 +1,12 @@
 package co.inventorsoft.academy.hibernate.dao.impl;
 
+import co.inventorsoft.academy.hibernate.dao.AbstractDao;
 import co.inventorsoft.academy.hibernate.dao.EmployeeDAO;
-import co.inventorsoft.academy.hibernate.model.Company;
 import co.inventorsoft.academy.hibernate.model.Employee;
-
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
 
-public class EmployeeDAOImpl implements EmployeeDAO {
-    private EntityManager entityManager;
-
+public class EmployeeDAOImpl extends AbstractDao<Employee> implements EmployeeDAO {
     public EmployeeDAOImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    @Override
-    public void saveEmployee(Employee employee) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            entityManager.persist(employee);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
-            throw e;
-        }
-    }
-
-    @Override
-    public Employee findById(Long id) {
-        return entityManager.find(Employee.class, id);
-    }
-
-    @Override
-    public void updateEmployee(Employee employee) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            entityManager.merge(employee);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
-            throw e;
-        }
-    }
-
-    @Override
-    public void deleteEmployee(Employee employee) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            if (entityManager.contains(employee)) {
-                entityManager.remove(employee);
-            } else {
-                entityManager.remove(entityManager.merge(employee));
-            }
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
-            throw e;
-        }
+        super(Employee.class, entityManager);
     }
 }
