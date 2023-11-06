f0e5d27fff9c48d3dd4cc3232993b44ba386f4f9
appsterlight <appsterlight@gmail.com>
2023-11-06 03:17:01 +0200

    Update getCompanyById() return type to Optional and add 'throws ServiceException' in method signatures

--- src/main/java/co/inventorsoft/academy/hibernate/service/CompanyService.java
+++ src/main/java/co/inventorsoft/academy/hibernate/service/CompanyService.java
@@ -1,10 +1,12 @@
 package co.inventorsoft.academy.hibernate.service;
 
+import co.inventorsoft.academy.hibernate.exception.ServiceException;
 import co.inventorsoft.academy.hibernate.model.Company;
+import java.util.Optional;
 
 public interface CompanyService {
-    void saveCompany(Company company);
-    Company getCompanyById(Long id);
-    void updateCompany(Company company);
-    void deleteCompany(Long id);
+    void saveCompany(Company company) throws ServiceException;
+    Optional<Company> getCompanyById(Long id);
+    void updateCompany(Company company) throws ServiceException;
+    void deleteCompany(Long id) throws ServiceException;
 }
