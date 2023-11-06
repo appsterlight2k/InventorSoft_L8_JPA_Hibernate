df817141af9f683ea47294ab6a6aaa0a368c031a
appsterlight <appsterlight@gmail.com>
2023-11-06 03:03:33 +0200

    - Optimize entity relationships and enforce constraints:
    Enhanced @OneToOne relationships with lazy loading.
    Added @NotNull annotations for mandatory fields. Updated column definitions to reflect nullable constraints. Removed  redundant constructors.

--- src/main/java/co/inventorsoft/academy/hibernate/model/Employee.java
+++ src/main/java/co/inventorsoft/academy/hibernate/model/Employee.java
@@ -2,12 +2,14 @@ package co.inventorsoft.academy.hibernate.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,21 +27,24 @@ public class Employee {
     @Id
     @Column(nullable = false)
     private Long id;
-    @Column(name = "first_name")
+
+    @NotNull
+    @Column(name = "first_name", nullable = false)
     private String firstname;
-    @Column(name = "last_name")
+
+    @NotNull
+    @Column(name = "last_name", nullable = false)
     private String lastname;
+
     private String phone;
+
+    @NotNull
+    @Column(nullable = false)
     private String email;
-    @OneToOne
-    @JoinColumn(name = "company_id")
+
+    @NotNull
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
     private Company company;
 
-    public Employee(String firstname, String lastname, String phone, String email, Company company) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.phone = phone;
-        this.email = email;
-        this.company = company;
-    }
 }
