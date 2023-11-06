9f751c2f89321574fae2f838d247956ece382718
appsterlight <appsterlight@gmail.com>
2023-11-06 03:17:31 +0200

    Update getEmployeeById() return type to Optional and add 'throws ServiceException' in method signatures

--- src/main/java/co/inventorsoft/academy/hibernate/service/EmployeeService.java
+++ src/main/java/co/inventorsoft/academy/hibernate/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package co.inventorsoft.academy.hibernate.service;
 
+import co.inventorsoft.academy.hibernate.exception.ServiceException;
 import co.inventorsoft.academy.hibernate.model.Employee;
+import java.util.Optional;
 
 public interface EmployeeService {
-    void saveEmployee(Employee employee);
-    Employee getEmployeeById(Long id);
-    void updateEmployee(Employee employee);
-    void deleteEmployee(Employee employee);
+    void saveEmployee(Employee employee) throws ServiceException;
+    Optional<Employee> getEmployeeById(Long id);
+    void updateEmployee(Employee employee) throws ServiceException;
+    void deleteEmployee(Employee employee) throws ServiceException;
 }
